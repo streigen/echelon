2b0d15c60d5f6d66b0ef816c1847a77d8006374b
Rayyan Parkar <rayyanparkar09@gmail.com>
2026-07-07 21:35:42 +0100

    saving prototype changes, still need to cleanup code and make everything more efficient

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
+use slint::Model;
 
 slint::include_modules!();
 
@@ -37,6 +38,27 @@ fn main() -> Result<(), Box<dyn Error>> {
         let _ui_handle = ui.as_weak();
         move || {
             println!("Forgot password");
+        }
+    });
+
+    ui.on_send_message({
+        let ui_handle = ui.as_weak();
+        move |msg_text| {
+            if let Some(ui) = ui_handle.upgrade() {
+                let current_messages = ui.get_messages();
+                let mut msgs: Vec<_> = current_messages.iter().collect();
+                
+                msgs.push(Message {
+                    user: slint::SharedString::from("Clumsy ☆"),
+                    time: slint::SharedString::from("just now"),
+                    text: slint::SharedString::from(msg_text),
+                    repliedTo: slint::SharedString::from(""),
+                    image: false,
+                });
+                
+                let new_model = std::rc::Rc::new(slint::VecModel::from(msgs));
+                ui.set_messages(new_model.into());
+            }
         }
     });
 
