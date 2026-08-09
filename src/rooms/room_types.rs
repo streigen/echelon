3faa72efadf9f447ecef47d2ebf654ef5fb22010
Siddhant Misra <me@flaxeneel2.net>
2026-08-09 22:37:56 +0100

    just use matrix room struct

--- src/rooms/room_types.rs
+++ src/rooms/room_types.rs
@@ -1,24 +1,7 @@
-use serde::{Deserialize, Serialize};
+use matrix_sdk::Room;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct RawRoom {
-    pub id: String,
-    pub name: Option<String>,
-    pub topic: Option<String>,
-    pub avatar_url: Option<String>,
-    pub is_space: bool
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone)]
 pub struct SpaceRoom {
-    #[serde(flatten)]
-    pub base: RawRoom,
-    pub parent_spaces: Vec<String>
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct DmRoom {
-    #[serde(flatten)]
-    pub base: RawRoom,
-    pub members: Vec<String>
+    pub room: Room,
+    pub children: Vec<Room>,
 }
