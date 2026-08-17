c41e9af22e0eddb1c3a1ed4e1fd4ff0035d83ea3
Siddhant Misra <me@flaxeneel2.net>
2026-08-17 01:13:53 +0100

    get rid of deprecated with_active_client closure function

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -10,20 +10,6 @@ pub mod media;
 pub mod messages;
 pub mod spaces;
 
-#[deprecated(
-    note = "loan architecture doesnt meld well with async closures. please no more lifetime hell"
-)]
-pub(crate) async fn with_active_client<T, F>(state: ClientState, f: F) -> Result<T, String>
-where
-    F: FnOnce(&ClientHandler) -> T,
-{
-    let state_r = state.read().await;
-    let Some(client_handler) = state_r.as_ref() else {
-        return Err("No active client session".to_string());
-    };
-    Ok(f(client_handler))
-}
-
 pub(crate) async fn get_active_client(state: &ClientState) -> Result<Client, String> {
     let state_r = state.read().await;
 
